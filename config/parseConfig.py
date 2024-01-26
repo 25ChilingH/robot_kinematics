331bd7c510998cbd9225011e2461a0fd6daa10ce
25ChilingH <25ChilingH@students.harker.org>
2024-01-26 09:33:14 -0800

    Change instructions.txt

--- config/parseConfig.py
+++ config/parseConfig.py
@@ -1,13 +1,16 @@
 def getPts(ptsTxtFile):
     points = []
+    seqs = []
     ptsFile = open(ptsTxtFile, "r")
     while True:
         line = ptsFile.readline().strip('\n')
         if not line:
             break
-        point = line.split(" ")
+        idx = line.find('(')
+        seqs.append(line[:idx])
+        point = line[idx+1:line.find(')')].split(',')
         points.append((float(point[0]), float(point[1])))
-    return points
+    return points, seqs
 
 def getSeqs(seqTxtFile):
     seqs = []
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import configparser
 DEFAULT_CONFIG_FILE = "./config/robot_map.cfg"
 KEYS = ["UNIT_LINE_SPACING", "DEG_ARC_SPACING",
         "DEG_TURN_SPACING", "SEC_CONTROL_LOOP",
-        "POINTS_FILE", "SEQUENCE_FILE"]
+        "POINTS_FILE"]
 
 config = configparser.ConfigParser()
 config.read(DEFAULT_CONFIG_FILE)
@@ -15,9 +15,7 @@ degPerDot = float(config.get('FLOAT', KEYS[1]))
 turnDegPerDot = float(config.get('FLOAT', KEYS[2]))
 delay = float(config.get('FLOAT', KEYS[3]))
 ptsFile = config.get('STRING', KEYS[4])
-seqFile = config.get('STRING', KEYS[5])
-points = parseConfig.getPts(ptsFile)
-sequences = parseConfig.getSeqs(seqFile)
+points, sequences = parseConfig.getPts(ptsFile)
 
 simulate(points, sequences, unitPerDot,
          degPerDot, turnDegPerDot, delay)
